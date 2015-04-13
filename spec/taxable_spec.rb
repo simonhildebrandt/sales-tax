362903e2150e1f5720eff6a9cb7fa045bc556458
Simon Hildebrandt <simonhildebrandt@gmail.com>
2015-04-13 12:54:39 +1000

    Added support for 5-cent-rounding and calculating sales tax

--- spec/taxable_spec.rb
+++ spec/taxable_spec.rb
@@ -1,11 +1,47 @@
 require 'spec_helper'
 require_relative '../taxable'
 
-describe Taxable do
-  let(:taxable_class) { Class.new { include Taxable } }
+class TaxableClass
+  include Taxable
 
+  def initialize(price)
+    @price = price
+  end
+
+  def shelf_price
+    @price
+  end
+end
+
+describe Taxable do
   context "calculates a 10% sales tax" do
-    let(:compact_disc) { taxable_class.new }
+    let(:compact_disc) { TaxableClass.new(14.99) }
     it { expect(compact_disc.sales_tax).to eq 1.50 }
+  end
+
+  context "rounds up to nearest five cents" do
+    # TODO: Consider cooking the values out and creating a loop
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.01)).to eq 0.05 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.04)).to eq 0.05 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.05)).to eq 0.05 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.06)).to eq 0.10 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.09)).to eq 0.10 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.10)).to eq 0.10 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.11)).to eq 0.15 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.15)).to eq 0.15 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.16)).to eq 0.20 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.19)).to eq 0.20 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.40)).to eq 0.40 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.41)).to eq 0.45 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.45)).to eq 0.45 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.46)).to eq 0.50 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.50)).to eq 0.50 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.50)).to eq 0.50 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.60)).to eq 0.60 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.95)).to eq 0.95 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.96)).to eq 1.00 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(0.99)).to eq 1.00 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(1.00)).to eq 1.00 }
+    it { expect(Taxable.round_up_to_nearest_five_cents(1.01)).to eq 1.05 }
   end
 end
--- taxable.rb
+++ taxable.rb
@@ -1,3 +1,13 @@
 module Taxable
+  def sales_tax
+    Taxable.round_up_to_nearest_five_cents(tax_value(shelf_price, 10))
+  end
 
+  def tax_value(price, tax)
+    price * tax * 0.01
+  end
+
+  def self.round_up_to_nearest_five_cents(value)
+    (value*20).ceil / 20.0
+  end
 end
